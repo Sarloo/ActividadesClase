6e790251f649cc108df6427996df10097a8162db
Sarloo <carloscecor05@gmail.com>
2025-09-02 15:14:09 -0600

    Create MazeSolver.java

---
+++ Laberinto/MazeSolver.java
@@ -0,0 +1,52 @@
+package Laberinto;
+
+public class MazeSolver {
+    static int[][] maze = {
+        {1,0,0,0},
+        {1,1,0,1},
+        {0,1,0,0},
+        {1,1,1,1}
+    };
+    static int [][] solution = new int [4][4];
+
+    public static boolean solve(int x, int y){
+        if (x == 3 && y == 3){
+            solution [x][y] = 1 ;
+            return true;
+        }
+        if (isSafe(x, y)){
+            solution[x][y] = 1;
+            if (solve(x + 1, y)){
+                return true;
+            }
+            if (solve(x, y + 1)){
+                return true;
+            }
+            solution[x][y] = 0; // backtracking
+            return false;
+        }
+
+        return false; 
+    }
+
+    static boolean isSafe(int x, int y) {
+        return x >= 0 && y >= 0 &&
+            x < 4 && y < 4 &&
+            maze[x][y] == 1;
+    }
+    static void printSolution() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(" " + solution[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        if (solve(0, 0)) {
+            printSolution();
+        } else {
+            System.out.println("No encontre el camino");
+        }
+    }
+}
